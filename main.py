56affb5b6d03e10c9568fc514053818f0ac031a0
DTE <dillon@paperspace.com>
2020-02-05 16:09:22 -0800

    lint

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import os
 import sys
 
-sys.path.append(path.abspath(path.join(path.dirname(__file__), "./utils")))
-sys.path.append(path.abspath(path.join(path.dirname(__file__), "./models")))
+sys.path.append(os.path.abspath(path.join(path.dirname(__file__), "./utils")))
+sys.path.append(os.path.abspath(path.join(path.dirname(__file__), "./models")))
 
 import argparse
 import time
